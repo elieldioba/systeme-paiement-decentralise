4b72f48413b1a442ca5d53fdbd7cf0f4fd8e41c3
elieldioba <elieldiobat@gmail.com>
2026-04-30 13:07:48 +0100

    Initialisation du projet et config H2/Security

---
+++ src/main/java/ga/inptic/monetique/systeme_paiement_decentralise/SystemePaiementDecentraliseApplication.java
@@ -0,0 +1,13 @@
+package ga.inptic.monetique.systeme_paiement_decentralise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemePaiementDecentraliseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemePaiementDecentraliseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ga/inptic/monetique/systeme_paiement_decentralise/SystemePaiementDecentraliseApplicationTests.java
@@ -0,0 +1,13 @@
+package ga.inptic.monetique.systeme_paiement_decentralise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemePaiementDecentraliseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
